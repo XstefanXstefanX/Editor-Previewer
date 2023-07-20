6bea461917ff3c622d3d7e9e81bfe498628b94f8
Stefan <stefan.babalici2006@gmail.com>
2023-07-20 19:12:35 +0300

    Created App.jsx/.css

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div className='App'>
+                
+            </div>
+        );
+    }
+}
+
+
+export default App;
