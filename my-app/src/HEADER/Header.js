dd42653ea05c7eb6f2d01140945e967a948db98a
Stefan <stefan.babalici2006@gmail.com>
2023-07-22 16:30:50 +0300

    update

--- my-app/src/HEADER/Header.js
+++ my-app/src/HEADER/Header.js
@@ -1,43 +1,42 @@
 import React from "react";
+import UpperHeader from "./UpperHeader";
+import { useState } from "react";
+import { menuItems } from "../menuItems";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBagShopping,
-  faUser,
-  faMagnifyingGlass,
-  faFlag,
-} from "@fortawesome/free-solid-svg-icons";
-
-class Header extends React.Component {
-  render() {
-    return (
-      <div id="nav-bar">
-        <ul id="list-menu">
-          <li>MyMarshall</li>
-          <li>Local Retailers</li>
-          <li>
-            EN|US <FontAwesomeIcon icon={faFlag} className="icon" />
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faUser} className="icon" /> Sign in
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faBagShopping} className="icon" /> 0
-          </li>
-        </ul>
-        <header>
-            <div className="nav-area">
-                <a href='/'>
-                    <img className='image' alt='logo' src='https://cdn.iconscout.com/icon/free/png-256/free-marshall-31-283429.png?f=webp'/>
-                </a>
-            </div>
-        </header>
-      </div>
-    );
-  }
-}
+const Header = () => {
+  const [drop, setDrop] = useState(false);
+  return (
+    <div id="nav-bar">
+      <UpperHeader />
+      <header>
+        <div className="nav-area">
+          <img
+            className="image"
+            alt="logo"
+            src="https://cdn.iconscout.com/icon/free/png-256/free-marshall-31-283429.png?f=webp"
+          />
+          <ul>
+            {menuItems.map((menuItem) => {
+              if (menuItem.submenu) {
+                return (
+                  <li>
+                    <button
+                      onMouseOver={() => setDrop((prev) => !prev)}
+                      onMouseOut={() => setDrop((prev) => !prev)}
+                      className="menuButton"
+                    >
+                      <a href={menuItem.url}>{menuItem.title}</a>
+                    </button>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
+      </header>
+      <div className={`drop ${drop ? "show" : ""}`}></div>
+    </div>
+  );
+};
 
 export default Header;
---
+++ my-app/src/HEADER/UpperHeader.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBagShopping,
+  faUser,
+  faMagnifyingGlass,
+  faFlag,
+} from "@fortawesome/free-solid-svg-icons";
+
+const UpperHeader = () => {
+  return (
+    <ul id="list-menu">
+      <li>MyMarshall</li>
+      <li>Local Retailers</li>
+      <li>
+        EN|US <FontAwesomeIcon icon={faFlag} className="icon" />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={faUser} className="icon" /> Sign in
+      </li>
+      <li>
+        <FontAwesomeIcon icon={faBagShopping} className="icon" /> 0
+      </li>
+    </ul>
+  );
+};
+
+export default UpperHeader;
