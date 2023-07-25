e1929475a23733e80cb3f1cedb5f03f32f713b5a
Stefan <stefan.babalici2006@gmail.com>
2023-07-25 20:59:47 +0300

    created file

---
+++ my-app/src/CONTENT/AnimatedParagraphs.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./AnimatedP.css";
+
+class AnimatedP extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <img
+            src="https://images.unsplash.com/photo-1613032970340-7846189c1cbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hcnNoYWxsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <p className="description">
+            Surrounded affronting favourable no mr. Lain knew like half she yet
+            joy. Be than dull as seen very shot. Attachment ye so am travelling
+            estimating projecting is. Off fat address attacks his besides.
+            Suitable settling mr attended no doubtful feelings. Any over for say
+            bore such sold five but hung.
+          </p>
+          <img
+            src="https://images.unsplash.com/photo-1589833870588-8a0902bcf55b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hcnNoYWxsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <p className="description">
+            Surrounded affronting favourable no mr. Lain knew like half she yet
+            joy. Be than dull as seen very shot. Attachment ye so am travelling
+            estimating projecting is. Off fat address attacks his besides.
+            Suitable settling mr attended no doubtful feelings. Any over for say
+            bore such sold five but hung.
+          </p>
+          <img
+            src="https://images.unsplash.com/photo-1582557678499-4d93b70f627b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1hcnNoYWxsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <p className="description">
+            Surrounded affronting favourable no mr. Lain knew like half she yet
+            joy. Be than dull as seen very shot. Attachment ye so am travelling
+            estimating projecting is. Off fat address attacks his besides.
+            Suitable settling mr attended no doubtful feelings. Any over for say
+            bore such sold five but hung.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+export default AnimatedP;
